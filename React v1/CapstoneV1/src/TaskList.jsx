2e908a293ca57887060bc351ff8dcd8f8750fdef
Nick-Zwarych <nzwarych2@gmail.com>
2025-02-26 16:54:21 -0500

    feat(task-list): Conditionally display task fields only when they have data

--- React v1/CapstoneV1/src/TaskList.jsx
+++ React v1/CapstoneV1/src/TaskList.jsx
@@ -11,36 +11,55 @@ function TaskList({ tasks }) {
         <ul>
           {tasks.map((task, index) => (
             <li key={index} style={{ marginBottom: "1em" }}>
+              {/* Task Title - Always Displayed */}
               <h3>{task.taskTitle}</h3>
-              <p>
-                <strong>On Date:</strong> {task.taskOnDate}
-              </p>
-              <p>
-                <strong>Before Date:</strong> {task.taskBeforeDate}
-              </p>
-              <p>
-                <strong>Date Flexible:</strong>{" "}
-                {task.taskIsDateFlexible ? "Yes" : "No"}
-              </p>
-              <p>
-                <strong>Time Required:</strong>{" "}
-                {task.taskTimeRequired ? "Yes" : "No"}
-              </p>
-              <p>
-                <strong>Time of Day:</strong> {task.taskTimeOfDay}
-              </p>
-              <p>
-                <strong>Location Type:</strong> {task.taskLocationType}
-              </p>
-              <p>
-                <strong>Location:</strong> {task.taskLocation}
-              </p>
-              <p>
-                <strong>Details:</strong> {task.taskDetails}
-              </p>
-              <p>
-                <strong>Budget:</strong> {task.taskBudget}
-              </p>
+
+              {/* Conditionally render fields only if they have a value */}
+              {task.taskOnDate && (
+                <p>
+                  <strong>On Date:</strong> {task.taskOnDate}
+                </p>
+              )}
+              {task.taskBeforeDate && (
+                <p>
+                  <strong>Before Date:</strong> {task.taskBeforeDate}
+                </p>
+              )}
+              {task.taskIsDateFlexible && (
+                <p>
+                  <strong>Date Flexible:</strong> Yes
+                </p>
+              )}
+              {task.taskTimeRequired && (
+                <p>
+                  <strong>Time Required:</strong> Yes
+                </p>
+              )}
+              {task.taskTimeOfDay && (
+                <p>
+                  <strong>Time of Day:</strong> {task.taskTimeOfDay}
+                </p>
+              )}
+              {task.taskLocationType && (
+                <p>
+                  <strong>Location Type:</strong> {task.taskLocationType}
+                </p>
+              )}
+              {task.taskLocation && (
+                <p>
+                  <strong>Location:</strong> {task.taskLocation}
+                </p>
+              )}
+              {task.taskDetails && (
+                <p>
+                  <strong>Details:</strong> {task.taskDetails}
+                </p>
+              )}
+              {task.taskBudget && (
+                <p>
+                  <strong>Budget:</strong> {task.taskBudget}
+                </p>
+              )}
             </li>
           ))}
         </ul>
