45a79d4a382590a4c998975aa8d3d6d91b7c66f0
Ryan H <ryan.david.hall99@gmail.com>
2025-03-26 16:15:51 -0400

    Quick Start Section for home page Created

--- React v1/CapstoneV1/src/Home.jsx
+++ React v1/CapstoneV1/src/Home.jsx
@@ -2,30 +2,34 @@
 import React from "react";
 import '../src/styles/Home.css';
 import heroImage from '../src/Images/heroImage1.png';
+import QuickStartSection from "../src/homeComponents/QuickStartSection";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="hero-section">
-      <div className="hero-content">
-        <h1 className="hero-title">GET MORE DONE</h1>
-        <p className="hero-subtext">Post any task. Pick the best person. Get it done.</p>
-        <div className="hero-buttons">
-          <Link to="/create">
-            <button className="btn primary">Post your task for free →</button>
-          </Link>
-          <button className="btn secondary">Earn money as a Tasker →</button>
+    <div>
+      <div className="hero-section">
+        <div className="hero-content">
+          <h1 className="hero-title">GET MORE DONE</h1>
+          <p className="hero-subtext">Post any task. Pick the best person. Get it done.</p>
+          <div className="hero-buttons">
+            <Link to="/create">
+              <button className="btn primary">Post your task for free →</button>
+            </Link>
+            <button className="btn secondary">Earn money as a Tasker →</button>
+          </div>
+          <div className="hero-stats">
+            <div><span>👥</span> 1M+ customers</div>
+            <div><span>🛡️</span> 2.5M+ tasks done</div>
+            <div><span>⭐</span> 4M+ user reviews</div>
+          </div>
         </div>
-        <div className="hero-stats">
-          <div><span>👥</span> 1M+ customers</div>
-          <div><span>🛡️</span> 2.5M+ tasks done</div>
-          <div><span>⭐</span> 4M+ user reviews</div>
-        </div>
-      </div>
 
-      <div className="hero-image">
-        <img src={heroImage} alt="Hero" className="hero-img" />
+        <div className="hero-image">
+          <img src={heroImage} alt="Hero" className="hero-img" />
+        </div>
       </div>
+      <QuickStartSection/>
     </div>
   );
 };
